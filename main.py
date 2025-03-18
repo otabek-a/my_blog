c2bbea76961474e8bb72150b8b9a5fe69fbe3604
otabek-a <otabekabdurasulov245@gmail.com>
2025-03-18 22:05:06 +0500

    qwerty

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ def main():
 
 
 if __name__=='__main__':
-#    app.run(debug=True, port=8800)
-   app.run(host='0.0.0.0', port=8100)   
+#   app.run(debug=True, port=8800)
+  app.run(host='0.0.0.0', port=8100)   
